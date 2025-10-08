028d1b2762cf2dd725c7261851c9ddf76cf2295a
LSmolnickiEstiCRM <50243583+lukasz-smolnicki@users.noreply.github.com>
2025-10-08 10:52:12 +0200

    fix(cd/web-e2e): doinstaluj @playwright/test (no-save) i uruchamiaj testy z configiem TS

--- apps/web/playwright.cloud.config.ts
+++ apps/web/playwright.cloud.config.ts
@@ -4,9 +4,7 @@ const config: PlaywrightTestConfig = {
   testDir: "./test",
   testMatch: ["**/*.spec.ts"],
   timeout: 60000,
-  use: {
-    baseURL: process.env.WEB_BASE_URL,
-  },
+  use: { baseURL: process.env.WEB_BASE_URL },
 };
 
 export default config;
